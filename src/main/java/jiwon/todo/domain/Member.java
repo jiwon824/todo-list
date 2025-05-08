240d72c973491a040c3a5bea43737594eb46caa8
jiwon824 <p01046108755@gmail.com>
2025-05-08 09:41:49 +0900

    [Feat] Member Entity 구현 (#3)

--- src/main/java/jiwon/todo/domain/Member.java
+++ src/main/java/jiwon/todo/domain/Member.java
@@ -1,4 +1,22 @@
 package jiwon.todo.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
 public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id", unique = true)
+    private Long id;
+
+    private String password;
+
+    private String name;
+
+    private List<Todo> todos = new ArrayList<>();
 }
