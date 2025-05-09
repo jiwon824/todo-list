c23f3388b7bb89776db31a17b47c8efbe3c5a56e
jiwon824 <p01046108755@gmail.com>
2025-05-09 11:44:21 +0900

    [Feat] MemberRepository 구현 (#3)

--- src/main/java/jiwon/todo/repository/MemberRepository.java
+++ src/main/java/jiwon/todo/repository/MemberRepository.java
@@ -1,4 +1,20 @@
 package jiwon.todo.repository;
 
+import jakarta.persistence.EntityManager;
+import jiwon.todo.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+    private final EntityManager em;
+
+    public void save(Member member){
+        em.persist(member);
+    }
+
+    public Member findById(Long id){
+        return em.find(Member.class, id);
+    }
 }
