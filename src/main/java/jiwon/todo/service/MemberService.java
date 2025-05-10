712539b098dd25dec0c3f065a551b5871485d0dd
jiwon824 <p01046108755@gmail.com>
2025-05-10 16:28:43 +0900

    [Feat] MemberService 구현 (#3)

--- src/main/java/jiwon/todo/service/MemberService.java
+++ src/main/java/jiwon/todo/service/MemberService.java
@@ -1,4 +1,34 @@
 package jiwon.todo.service;
 
+import jiwon.todo.domain.Member;
+import jiwon.todo.domain.Todo;
+import jiwon.todo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
+
+    private MemberRepository memberRepository;
+
+    // 회원가입
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        // id 중복 검사
+        // EXCEPTION
+    }
+
+    public Member findById(Long id){
+        return memberRepository.findById(id);
+    }
+
 }
