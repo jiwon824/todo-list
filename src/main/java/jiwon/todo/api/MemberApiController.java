f423b1696fa84f7c3b2760291c6a84e44f442476
jiwon824 <p01046108755@gmail.com>
2025-05-18 17:07:37 +0900

    [Feat] MemberApiController saveMember 구현 (#3)

--- src/main/java/jiwon/todo/api/MemberApiController.java
+++ src/main/java/jiwon/todo/api/MemberApiController.java
@@ -1,4 +1,43 @@
 package jiwon.todo.api;
 
+import jakarta.validation.Valid;
+import jiwon.todo.domain.Member;
+import jiwon.todo.service.MemberService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // @@Controller + @ResponseBody
+@RequiredArgsConstructor
 public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/members")
+    public CreateMemberResponse saveMember(@RequestBody @Valid CreateMemberRequest request){
+        Member member = new Member(request.getLoginId(), request.getPassword(),
+                                    request.getName(), request.getEmail());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String loginId;
+        private String password;
+        private String name;
+        private String email;
+    }
 }
