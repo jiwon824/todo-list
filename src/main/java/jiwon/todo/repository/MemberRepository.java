cbac1745fd60b41d805a87d4368d1c78e8259cff
jiwon824 <p01046108755@gmail.com>
2025-05-23 14:00:14 +0900

    [Feat] MemberRepository existsBy 함수 구현 (#3)

--- src/main/java/jiwon/todo/repository/MemberRepository.java
+++ src/main/java/jiwon/todo/repository/MemberRepository.java
@@ -17,4 +17,18 @@ public class MemberRepository {
     public Member findById(Long id){
         return em.find(Member.class, id);
     }
+
+    public boolean existsByLoginId(String loginId){
+        Long count = em.createQuery("select count(m) from Member m where m.loginId = :loginId", Long.class)
+                .setParameter("loginId", loginId)
+                .getSingleResult();
+        return count > 0;
+    }
+
+    public boolean existsByEmail(String email){
+        Long count = em.createQuery("select count(m) from Member m where m.email = :email", Long.class)
+                .setParameter("email", email)
+                .getSingleResult();
+        return count > 0;
+    }
 }
