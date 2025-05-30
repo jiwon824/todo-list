e4f72e3726f37034c4fafecbd8f6d7d17fb4163e
jiwon824 <p01046108755@gmail.com>
2025-05-30 11:10:13 +0900

    [Fix] API 유효성 검증이 되지 않던 문제 수정 (#3)

--- src/main/java/jiwon/todo/api/MemberApiController.java
+++ src/main/java/jiwon/todo/api/MemberApiController.java
@@ -1,6 +1,9 @@
 package jiwon.todo.api;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import jiwon.todo.domain.Member;
 import jiwon.todo.service.MemberService;
 import lombok.Data;
@@ -35,9 +38,21 @@ public class MemberApiController {
 
     @Data
     static class CreateMemberRequest {
+        @NotEmpty(message = "이름을 입력해주세요")
+        private String name; // 이름 혹은 닉네임
+
+        @NotEmpty(message = "ID를 입력해주세요")
         private String loginId;
-        private String password;
-        private String name;
+
+        @NotEmpty(message = "비밀번호를 입력해주세요")
+        @Pattern(message = "비밀번호 형식이 올바르지 않습니다.",
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$")
+        private String password; // 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함
+
+        @NotEmpty(message = "이메일을 입력해주세요")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        @Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$",
+                message = "도메인은 최소 하나 이상의 점(.)을 포함해야 합니다.")
         private String email;
     }
 }
