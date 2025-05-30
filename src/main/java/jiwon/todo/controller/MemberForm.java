e80c9cef8c88d0b9514741bba41155c8483a43c9
jiwon824 <p01046108755@gmail.com>
2025-05-30 11:17:14 +0900

    [Fix] MemberForm 이메일 유효성 검증 수정 (#3)

--- src/main/java/jiwon/todo/controller/MemberForm.java
+++ src/main/java/jiwon/todo/controller/MemberForm.java
@@ -13,13 +13,15 @@ public class MemberForm {
 
     @NotEmpty(message = "ID를 입력해주세요")
     private String loginId;
+
     @NotEmpty(message = "비밀번호를 입력해주세요")
     @Pattern(message = "비밀번호 형식이 올바르지 않습니다.",
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#])[A-Za-z\\d@$!%*?&#]{8,}$")
     private String password; // 8자 이상, 대소문자 포함, 숫자 및 특수문자(@$!%*?&#) 포함
 
     @NotEmpty(message = "이메일을 입력해주세요")
-    @Email(message = "이메일 형식이 올바르지 않습니다.",
-            regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Pattern(message = "도메인은 최소 하나 이상의 점(.)을 포함해야 합니다.",
+            regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
     private String email;
 }
